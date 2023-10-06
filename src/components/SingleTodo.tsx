3a4a728db1ec275d8dbbb8726a2f9c31887f1700
Javier Alaves <jayalaves@gmail.com>
2023-10-06 18:15:26 +0200

    edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -11,6 +11,9 @@ type Props = {
 };
 
 const SingleTodo = ({ todos, todo, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -25,16 +28,57 @@ const SingleTodo = ({ todos, todo, setTodos }: Props) => {
     setTodos(todos.filter((todo) => todo.id != id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  // to focus on input on edit
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // switch to input focus on command
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="todos_single">
-      {todo.isDone ? (
-        <s className="todos_single--text">{todo.todo}</s>
-      ) : (
-        <span className="todos_single--text">{todo.todo}</span>
-      )}
+    <form className="todos_single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {
+        // check if edit mode is on
+        edit ? (
+          // edit mode is on
+          <input
+            // reference for focus on edit
+            ref={inputRef}
+            // value from editTodo useState
+            value={editTodo}
+            // when pressing enter it'll change the todo
+            onChange={(e) => setEditTodo(e.target.value)}
+            // styling
+            className="todos_single--text"
+          />
+        ) : // edit mode not on
+        todo.isDone ? (
+          <s className="todos_single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos_single--text">{todo.todo}</span>
+        )
+      }
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            // if not edit mode and not done
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
